5b3666708365b5637d3f48d4d9ddc99989b144da
Luco06 <luc-olivier.yohan@hotmail.fr>
2022-11-18 15:14:47 +0100

    Suppression des img de l'object

--- Api/Mock/StepPresentations.js
+++ Api/Mock/StepPresentations.js
@@ -4,25 +4,21 @@ const StepPresentation = [
       id: "1",
       title: "Crée/Rejoins des DelingGroup",
       text: "Crée ou rejoins une communauté de DelingUser",
-      img: require("../../assets/Img_Presentation/ImgPrez1.png"),
     },
     step2: {
       id: "2",
       title: "Partage ton expérience !",
       text: "Fait  leurs aimer ou détester ton livre du moment !",
-      img: require("../../assets/Img_Presentation/ImgPrez2.png"),
     },
     step3: {
       id: "3",
       title: "Découvre des pépites !",
       text: "Découvré les livres du monents des autres DelingUser et discutez-en !",
-      img: require("../../assets/Img_Presentation/ImgPrez3.png"),
     },
     step4: {
       id: "4",
       title: "Troc ton livre !",
       text: "Vous avez aimé le récit d’un autre DelingUser et lui le votre? Échangez vos livres !",
-      img: require("../../assets/Img_Presentation/ImgPrez4.png"),
     },
   },
 ];
