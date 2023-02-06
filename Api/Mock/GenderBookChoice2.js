0c5267ff30cbf53643c0e971ad269ed79dac6b8f
Luco06 <luc-olivier.yohan@hotmail.fr>
2023-02-06 12:13:57 +0100

    Add mock choice book

---
+++ Api/Mock/GenderBookChoice2.js
@@ -0,0 +1,15 @@
+const GenderBookChoice2 = [
+  {
+    id: 4,
+    txt: "Descriptifs",
+  },
+  {
+    id: 5,
+    txt: "Narratifs",
+  },
+  {
+    id: 6,
+    txt: "Graphiques",
+  },
+];
+export default GenderBookChoice2;
