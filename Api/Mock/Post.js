9eb891aeef307160e5b3e1850ab3d1a2d9a46014
Luco06 <luc-olivier.yohan@hotmail.fr>
2023-05-02 18:03:49 +0200

    Ajout de mockApi

---
+++ Api/Mock/Post.js
@@ -0,0 +1,35 @@
+const Post = [
+  {
+    id: 1,
+    img: require("../../assets/Img_story/Avatar5.png"),
+    pseudo: "BookLover30",
+    bookImg: require("../../assets/Img_Presentation/bookUser.png"),
+    comment:
+      "Je  viens de découvrir un livre magnifiquement bien écrit et très émouvant, je recommande !!!👌😱😢#LaromancedelaGoulue",
+    like: 120,
+    coms: 66,
+    share: 15,
+  },
+  {
+    id: 2,
+    img: require("../../assets/Img_story/MyAvatar.png"),
+    pseudo: "DcLover17",
+    bookImg: require("../../assets/Img_Presentation/tokyo.png"),
+    comment:
+      "Comic indépendant à dévorer dans les plus bref delais !!#Tokyogosth",
+    like: 110,
+    coms: 90,
+    share: 30,
+  },
+  {
+    id: 3,
+    img: require("../../assets/Img_story/Avatar2.png"),
+    pseudo: "Urbanreader",
+    bookImg: require("../../assets/Img_Presentation/scum.png"),
+    comment: "Incroyable !!#thescumbag",
+    like: 160,
+    coms: 120,
+    share: 60,
+  },
+];
+export default Post;
