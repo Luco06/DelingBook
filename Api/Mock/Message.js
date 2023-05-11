512c0a8e02655e56c82e75ccd049e378ea03f509
Luco06 <luc-olivier.yohan@hotmail.fr>
2023-05-11 17:38:42 +0200

    Ajout des Mocks Api

---
+++ Api/Mock/Message.js
@@ -0,0 +1,18 @@
+const Message = [
+  {
+    id: 1,
+    img: require("../../assets/Img_story/Avatar1.png"),
+    pseudo: "Reader",
+    mess1:
+      "Il a l’air excellent celuis la aussi, mais je ne l’ai pas trouvée...Ça te  dit un  échange?  je te laisse regarder ma bibliothèque.@Dreamspace",
+    mess2: "Cool!! Merci je regarde sa page",
+    mess3: "HOui bah c’est pour ça😂😂",
+    mess4: "J’adore ! Surtout le héro.",
+    mess5: "Hello, j’ai lu ta recommandation !!",
+    rep1: "#Theweatherman , je  pense  que tu  vas bien aimer",
+    rep2: "Je t’en envoie une autre de recommandation.",
+    rep3: "Ouais il  est un peu stupide😂",
+    rep4: "Alors? tu en pense quoi?",
+  },
+];
+export default Message;
